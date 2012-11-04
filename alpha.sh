ab3b7807eeeb63c4dec23669acf2d919eb0069a4
dijeferson <dijeferson@gmail.com>
2012-11-04 21:37:24 -0200

    File names with spaces fixed
    Issue when using files with spaces in its name fixed.

--- alpha.sh
+++ alpha.sh
@@ -5,53 +5,60 @@
 # @dijeferson / dijeferson@gmail.com
 
 # Inicio das variaveis
-movie=$1
+movie="$1";
 echo $movie
-h264file=$movie".264"
-dtsfile=$movie".dts"
-ac3file=$movie".ac3"
-aacfile=$movie".aac"
-aac6file=$movie".6.aac"
-moviem4v=$movie".m4v"
+h264file=$movie".264";
+dtsfile=$movie".dts";
+ac3file=$movie".ac3";
+aacfile=$movie".aac";
+aac6file=$movie".6.aac";
+moviem4v=$movie".m4v.chk";
 
 
 # Grava dados do video
-/Applications/Mkvtoolnix.app/Contents/MacOS/mkvinfo $movie > .mediainfo
+/Applications/Mkvtoolnix.app/Contents/MacOS/mkvinfo "$movie" > .mediainfo
+mediainfo "$movie" > .mediainfo2
+
 mediainforesult=$(tail .mediainfo)
+mediainforesult2=$(tail .mediainfo2)
 
 # Extrai trilha de video
 #echo 'Extracting Video...'
 #/Applications/Mkvtoolnix.app/Contents/MacOS/mkvextract tracks $movie 0:$h264file
 #echo 'Video Extracted [Ok]'
 
+notstereo='6 channels';
+
 # Faz as conversoes 
 if [ "$mediainforesult" = "(MKVInfo) No EBML head found." ]; then
-    echo 'Converting to AAC Stereo HQ'
-    ffmpeg -i $movie -acodec copy $aac6file
-    faad -d -o $aacfile.tmp $aac6file
-    ffmpeg -i $aacfile.tmp -acodec libfaac $aacfile
-    echo 'Finished Converted'
+	# verifica se a trilha atual já é AAC Stereo - se nao for, converte
+    if [[ "$mediainforesult2" == *"$notstereo"* ]]; then
+	    echo 'Converting to AAC Stereo HQ'
+    	ffmpeg -i "$movie" -acodec copy "$aac6file"
+    	faad -d -o "$aacfile.tmp" "$aac6file"
+    	ffmpeg -i "$aacfile.tmp" -acodec libfaac "$aacfile"
+    	echo 'Finished Converted'
+	fi
 else
     # Obtem trilha de DTS/AC3
-    trackDTS=$(more .mediainfo 2>&1 | grep -B3 A_DTS | awk '{print substr($0,index($0,"mkvextract:")+12,1)}' | head -n 1)
-    trackAC3=$(more .mediainfo 2>&1 | grep -B3 A_AC3 | awk '{print substr($0,index($0,"mkvextract:")+12,1)}' | head -n 1)
+    trackDTS=$(cat .mediainfo 2>&1 | grep -B3 A_DTS | awk '{print substr($0,index($0,"mkvextract:")+12,1)}' | head -n 1)
+    trackAC3=$(cat .mediainfo 2>&1 | grep -B3 A_AC3 | awk '{print substr($0,index($0,"mkvextract:")+12,1)}' | head -n 1)
 
     # Se houver trilha dts entao extraia
     if [ ! -z "$trackDTS" ]; then
         echo 'Extracting DTS...'
-        /Applications/Mkvtoolnix.app/Contents/MacOS/mkvextract tracks $movie $trackDTS:$dtsfile
+        /Applications/Mkvtoolnix.app/Contents/MacOS/mkvextract tracks "$movie" "$trackDTS":"$dtsfile"
         echo 'DTS Extracted [Ok]'
         
         echo 'Converting DTS to AC3'
-        dcadec -o wavall $dtsfile | aften - $ac3file
+        dcadec -o wavall "$dtsfile" | aften - "$ac3file"
         echo 'Convertion completed [Ok]'
 
     else
-
         # Se houver trilha ac3 entao extraia
         if [ ! -z "$trackAC3" ]; then
             echo 'Extracting AC3...'
-            /Applications/Mkvtoolnix.app/Contents/MacOS/mkvextract tracks $movie $trackAC3:$ac3file
+            /Applications/Mkvtoolnix.app/Contents/MacOS/mkvextract tracks "$movie" "$trackAC3":"$ac3file"
             echo 'AC3 Extracted [Ok]'
         fi
     fi
@@ -59,15 +66,15 @@ else
     # Convertendo de AC3 5.1 para AAC Stereo HQ
     echo 'Converting AC3 to AAC Stereo HQ'
     #ffmpeg -i $ac3file -acodec libfaac -ac 2 -ar 48000 -ab 192k $aacfile
-    ffmpeg -i $ac3file -acodec libfaac $aacfile
+    ffmpeg -i "$ac3file" -acodec libfaac -ac 2 "$aacfile"
     echo 'Convertion AC3 to AAC Stereo HQ [Ok]'
 fi
 
 # Obtendo legendas en-us, pt-br, es-es
 echo 'Getting Subtitles in English, Portugues, and Spanish'
-/Applications/FileBot.app/Contents/MacOS/filebot -get-subtitles $movie --lang pb --output srt --encoding utf8 -non-strict
-/Applications/FileBot.app/Contents/MacOS/filebot -get-subtitles $movie --lang en --output srt --encoding utf8 -non-strict
-/Applications/FileBot.app/Contents/MacOS/filebot -get-subtitles $movie --lang es --output srt --encoding utf8 -non-strict
+/Applications/FileBot.app/Contents/MacOS/filebot -get-subtitles "$movie" --lang pb --output srt --encoding utf8 -non-strict
+/Applications/FileBot.app/Contents/MacOS/filebot -get-subtitles "$movie" --lang en --output srt --encoding utf8 -non-strict
+/Applications/FileBot.app/Contents/MacOS/filebot -get-subtitles "$movie" --lang es --output srt --encoding utf8 -non-strict
 echo 'Subtitltes [Ok]'
 
 #Remuxando MKV->MP4
@@ -75,21 +82,26 @@ echo 'Subtitltes [Ok]'
 
 echo 'Remuxing...'
 if [ ! -z ls $ac3file ]; then
-    echo ffmpeg -i $movie -i $ac3file -i $aacfile -vcodec copy -acodec copy -acodec copy $moviem4v -map 0.0 -map 1.0 -map 2.0 -newaudio -threads 4
-    ffmpeg -i $movie -i $ac3file -i $aacfile -vcodec copy -acodec copy -acodec copy $moviem4v -map 0.0 -map 1.0 -map 2.0 -newaudio -threads 4
+    #echo ffmpeg -i $movie -i $ac3file -i $aacfile -vcodec copy -acodec copy -acodec copy $moviem4v -map 0.0 -map 1.0 -map 2.0 -newaudio -threads 4
+    ffmpeg -i "$movie" -i "$ac3file" -i "$aacfile" -vcodec copy -acodec copy -acodec copy $moviem4v -map 0.0 -map 1.0 -map 2.0 -newaudio -threads 4
 else
-    echo ffmpeg -i $movie -i $aacfile -vcodec copy -acodec copy $moviem4v -map 0.0 -map 1.0 -threads 4
-    ffmpeg -i $movie -i $aacfile -vcodec copy -acodec copy $moviem4v -map 0.0 -map 1.0 -threads 4
+#    echo ffmpeg -i $movie -i $aacfile -vcodec copy -acodec copy $moviem4v -map 0.0 -map 1.0 -threads 4
+    ffmpeg -i "$movie" -i "$aacfile" -vcodec copy -acodec copy "$moviem4v" -map 0.0 -map 1.0 -newaudio -threads 4
 fi
+
 echo 'Finished remuxing!'
 
-echo 'Getting Metadata'
+pwd > .curdir
+currentDir=$(cat .curdir)
+
+osascript -e "tell application \"Finder\" to set label index of alias POSIX file \"$currentDir\" to 6"	
+
+growlnotify -m "Finished encoding movie $movie" -n "ATV Media Converter"
 
 
+#echo 'Getting Metadata'
 #python -c 'import json,sys;obj=sys.stdin;print obj[0:obj.index("720")].replace("."," ")'
-
 #curl http://www.imdbapi.com/?t="$movie" > .imdbinfo
-
 #title=$(more .imdbinfo | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Title"]')
 #year=$(more .imdbinfo | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Year"]')
 #rated=$(more .imdbinfo | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Rated"]')
@@ -100,15 +112,17 @@ echo 'Getting Metadata'
 #actors=$(more .imdbinfo | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Actors"]')
 #plot=$(more .imdbinfo | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Plot"]')
 #poster=$(more .imdbinfo | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Poster"]')
-
 #curl $poster > .poster.jpg
 
-SublerCLI -source $movie.pb.srt -language "Portuguese" -remove -dest $moviem4v
-SublerCLI -source $movie.en.srt -language "English"  -dest $moviem4v.m4v 
-SublerCLI -source $movie.spa.srt -language "Spanish" -dest $moviem4v.m4v -optimize
+#SublerCLI -source $movie.pb.srt -language "Portuguese" -remove -dest $moviem4v
+#SublerCLI -source $movie.en.srt -language "English"  -dest $moviem4v.m4v 
+#SublerCLI -source $movie.spa.srt -language "Spanish" -dest $moviem4v.m4v -optimize
 
-echo 'Metadata gathered'
+#echo 'Metadata gathered'
 
-echo 'Adding movie to your iTunes Library'
-echo osascript -e 'tell application "iTunes" to add file "' $moviem4v '" to playlist "Library" of source "Library"'
-echo 'Movie added to iTunes Library [Ok]'
+#echo 'Adding movie to your iTunes Library'
+#echo osascript -e 'tell application "iTunes" to add file "' $moviem4v '" to playlist "Library" of source "Library"'
+#echo 'Movie added to iTunes Library [Ok]'
+
+
+
